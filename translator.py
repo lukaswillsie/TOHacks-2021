986ae74f1093a368e08d7c0574f84628e43aa053
Vaskar <vaskar.nath@outlook.com>
2021-05-08 09:48:04 -0700

    ads

--- translator.py
+++ translator.py
@@ -1,14 +1,12 @@
 import nltk
 from nltk import word_tokenize, pos_tag
 from nltk.corpus import wordnet
-
 from IPython.display import display
-
 from stat_parser import Parser
 
 
 for ss in wordnet.synsets('sum'):
     print(ss.name(), ss.lemma_names())
 
-parser = Parser()
-print(parser.parse("Find the sum from 1 to 100"))
+operation_word_map = {}    
+
