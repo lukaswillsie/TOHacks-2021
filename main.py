95b05d1a5817edb62167f24b4a68fd1ce28173f8
Lukas Willsie <lukaswillsie@gmail.com>
2021-05-08 16:00:49 -0300

    Add simple Flask setup

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/ping")
+def ping():
+    return request.args.get("val")
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
