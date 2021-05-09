7fcf4fcc18dde7db6946f505ea7b269e173cb03e
Vaskar <vaskar.nath@outlook.com>
2021-05-08 21:40:49 -0700

    chages

--- parser.py
+++ parser.py
@@ -11,7 +11,9 @@ synonym_dict = {
     'subtract' : ('minus'),
     'multiply' : ('product'),
     'divide' : (),
-    'power' : ('exponentiate')
+    'power' : ('exponentiate'),
+    'about' : ('regarding', 'concerning'),
+    'summarize' : ('summarise'),
     }
 
 class TextParser:
@@ -26,22 +28,26 @@ class TextParser:
             'power'
         )
         self.ops = (
-            'summarize'
+            'summarize',
+            'translate'
         )
         custom_sent_tokenizer = PunktSentenceTokenizer(text)
         tokenized = custom_sent_tokenizer.tokenize(text)
         words = nltk.word_tokenize(tokenized[0])
         self.parsed_text = nltk.pos_tag(words)
-        self.verb = ''
+        self.verb = []
+        self.verb_ranges = []
 
     def extract_verb_math(self):
+        ids = []
+        verbs = []
         for i in range(len(self.parsed_text)):
             word = self.parsed_text[i]
             if 'N' in word[1] or 'V' in word[1] or "R" in word[1] or "J" in word[1]:
                 for ops in self.math_ops:
                     if self.equals(ops, word[0]):
-                        self.verb = ops
-                        return ops
+                        verbs.append(ops)
+                        ids.append(i)
             try:
                 temp = [self.parsed_text[j][1] for j in range(i,i+3)]
             except:
@@ -49,27 +55,40 @@ class TextParser:
             if temp == ['IN', 'DT', 'NN']:
                 for ops in self.math_ops:
                     if self.equals(ops, self.parsed_text[i + 2][0]):
-                        self.verb = ops
-                        return ops
-        return None
+                        verbs.append(ops)
+                        ids.append(i)
+        if len(ids) == 0:
+            return None
+        else:
+            self.verb_ranges = ids
+            self.verb = verbs
+            return self.verb
     
     def extract_verb_summary(self):
         self.verb = 'summarize'
         return 'summarize'
     
     def extract_verb(self):
-        temp = self.get_synonyms('summarise').union(self.get_synonyms('find'))
+        temp = self.get_synonyms('summarize').union(self.get_synonyms('find'))
         for word in self.parsed_text:
-            if word[0] in temp:
-                self.verb = 'summarize'
-                return self.extract_verb_summary() 
+            for ops in self.ops:
+                if self.equals(ops, word[0]):
+                    self.verb = ops
+                    return ops
         return self.extract_verb_math()
 
     def collect_args_math(self):
+        res = []
+        for j in range(len(self.verb_ranges) - 1):
+            res.append(self.collect_args_math_helper(self.verb_ranges[j] + 1, self.verb_ranges[j + 1]))
+        res.append(self.collect_args_math_helper(self.verb_ranges[-1], len(self.parsed_text)))
+        return res
+
+    def collect_args_math_helper(self, start, end):
         idx = []
         vals = []
         res = []
-        for i in range(len(self.parsed_text)):
+        for i in range(start, end):
             if self.parsed_text[i][1] == 'CD':
                 idx.append(i)
                 vals.append(int(self.parsed_text[i][0]))
@@ -83,13 +102,13 @@ class TextParser:
                 res.append(range(vals[i], vals[i + 1] + 1))
                 i += 1
             else:
-                res.append(vals[i])
+                res.append(vals[i]) 
             i += 1
         return res
     
     def collect_args_summary(self):
-        temp = [item[1] for item in self.parsed_text]
-        idx = max(loc for loc, val in enumerate(temp) if val == 'IN')
+        temp = [item[0] for item in self.parsed_text]
+        idx = max(loc for loc, val in enumerate(temp) if self.equals('about', val))
         temp_res = self.parsed_text[idx+1:]
         res = []
         s = ''
@@ -103,9 +122,20 @@ class TextParser:
             res.append(s.strip())
         return res
     
+    def collect_args_translate(self):
+        temp = [item[0] for item in self.parsed_text]
+        languages = ['english', 'french', 'spanish', 'korean']
+        for word in self.parsed_text:
+            if word[0] in languages:
+                language = word[0]
+        idx = max(loc for loc, val in enumerate(temp) if val == language)
+        return " ".join(temp[idx + 1:])
+    
     def collect_args(self):
         if self.verb == 'summarize':
             return self.collect_args_summary()
+        if self.verb == 'translate':
+            return self.collect_args_translate()
         else:
             return self.collect_args_math()
 
@@ -123,12 +153,13 @@ class TextParser:
 
 
 if __name__ == "__main__":
-    text = "add 4 to 10"
+    text = "translate to english blah blah blah"
     custom_sent_tokenizer = PunktSentenceTokenizer(text)
     tokenized = custom_sent_tokenizer.tokenize(text)
     words = nltk.word_tokenize(tokenized[0])
     tagged = nltk.pos_tag(words)
     print(tagged)
     parser = TextParser(text)
+    print(parser.get_synonyms('about'))
     print(parser.extract_verb())
     print(parser.collect_args())
