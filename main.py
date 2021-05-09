cb0e69725a4a3f3720c60a9d76c6731ddf00f462
Lukas Willsie <lukaswillsie@gmail.com>
2021-05-09 03:45:46 -0300

    CORS headers and JSON response body

--- main.py
+++ main.py
@@ -1,37 +1,19 @@
-<<<<<<< HEAD
 from flask import Flask, request
 
 app = Flask(__name__)
 
-
-@app.route("/ping")
-def ping():
-    return request.args.get("val")
+BASE_HEADERS = {"Access-Control-Allow-Origin": '*'}
 
 
-@app.route('/')
-def hello():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
-=======
-from flask import Flask, request
-
-app = Flask(__name__)
-
-
-@app.route("/ping")
-def ping():
-    return request.args.get("val")
+@app.route("/ping/<text>")
+def ping(text):
+    return {"result": text.upper()}, BASE_HEADERS
 
 
 @app.route('/')
 def hello():
-    return {'data': 'Hello World!'}, {"Access-Control-Allow-Origin": '*'}
+    return {'data': 'Hello World!'}, BASE_HEADERS
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
->>>>>>> e2c8619121723c04358e36195ef3ef15930e1c8e
